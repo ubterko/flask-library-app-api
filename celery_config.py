c3bfc8940a1cd2eab736b7088afd1325ee8ae721
ubterko <ebonkoisrael@gmail.com>
2024-09-13 23:07:36 +0100

    celery worker update

--- celery_config.py
+++ celery_config.py
@@ -1,6 +1,9 @@
 from celery import Celery
-from frontend_api.models import Book
+
+from frontend_api.models import Book as book_frontend_tb
 from frontend_api.extensions import db
+
+from admin_api.models import User as user_admin_tb
 
 app = Celery('bookstore', broker='redis://localhost:6379/0',
              backend='redis://localhost:6379/0')
@@ -9,7 +12,7 @@ app = Celery('bookstore', broker='redis://localhost:6379/0',
 def update_bookstore(payload):
     for book in payload:
         db.session.add(
-            Book(
+            book_frontend_tb(
                 title=payload.get("title"),
                 author=payload.get("author"),
                 category=payload.get("category"),
